dd842f227480db244a5e80a4c441ca419c1350bb
Farhad Hajnoruzi <farhad.hajnoruzi@gmail.com>
2022-02-23 15:16:25 +0100

    Adding right content and showing the content in html

---
+++ scripts/endingScript.js
@@ -0,0 +1,14 @@
+function openContent(evt, contentName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(contentName).style.display = "block";
+    // evt.currentTarget.className += " active";
+  }
